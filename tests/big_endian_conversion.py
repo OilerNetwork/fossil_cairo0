0c36de942988a256cbf5460b2427ab1cc9aaa988
Marcello Bardus <marcello@bardus.it>
2021-11-25 17:55:26 +0100

    proper convertion asserted

--- tests/big_endian_conversion.py
+++ tests/big_endian_conversion.py
@@ -3,6 +3,8 @@ import pytest
 from starkware.starknet.testing.contract import StarknetContract
 from starkware.starknet.testing.starknet import Starknet
 
+from utils.helpers import string_to_byte
+
 
 class TestsDeps(NamedTuple):
     starknet: Starknet
@@ -17,3 +19,12 @@ async def setup():
 @pytest.mark.asyncio
 async def test_to_big_endian():
     starknet, converter = await setup()
+
+    input_str = 'f90218a0'
+    little_endian_input = string_to_byte(input_str)
+
+    convert_call = await converter.test_to_big_endian(little_endian_input).call()
+    output = convert_call.result.res
+
+    assert output == int.from_bytes(input_str.encode("UTF-8"), 'big')
+
