5c7a8471ce784ba146b087d77c766d856958bfff
Marcello Bardus <marcello@bardus.it>
2021-12-07 10:41:15 +0000

    nibbles

--- tests/lib_local_patricia_tries.py
+++ tests/lib_local_patricia_tries.py
@@ -1,7 +1,7 @@
 import pytest
 from utils.encode_proof import encode_proof
 from mocks.trie_proofs import trie_proofs
-from utils.helpers import word64_to_bytes, word64_to_bytes_recursive
+from utils.helpers import word64_to_nibbles
 
 @pytest.mark.asyncio
 async def test_encode_proof():
@@ -11,9 +11,9 @@ async def test_encode_proof():
 
 @pytest.mark.asyncio
 async def test_word64_to_bytes():
-    word = 17259722129448184887
+    word = 12379813738319119365
     print('\n')
-    res = word64_to_bytes(word, 8)
-    print(f'res regular: {res}')
-    res_recursive = word64_to_bytes_recursive(word, 8)
-    print(f'res recursive: {res_recursive}')
+    res = word64_to_nibbles(word, 15)
+    print(res)
+    
+    
