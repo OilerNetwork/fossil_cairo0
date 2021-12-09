d5ab2c7017d55b47a9ec1e92f8e8506eabf2d2fc
Marcello Bardus <marcello@bardus.it>
2021-12-09 18:08:52 +0000

    solidity contracts tested

--- contracts/ethereum/L1MessagesSender.sol
+++ contracts/ethereum/L1MessagesSender.sol
@@ -1,28 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.7;
+pragma solidity 0.8.9;
 
-interface IStarknetCore {
-    /**
-      Sends a message to an L2 contract.
-    */
-    function sendMessageToL2(
-        uint256 to_address,
-        uint256 selector,
-        uint256[] calldata payload
-    ) external;
+import {FormatWords64} from "./lib/FormatWords64.sol";
+import {IStarknetCore} from "./interfaces/IStarknetCore.sol";
 
-    /**
-      Consumes a message that was sent from an L2 contract.
-    */
-    function consumeMessageFromL2(uint256 fromAddress, uint256[] calldata payload) external;
-}
 
 contract L1MessagesSender {
     IStarknetCore public immutable starknetCore;
     uint256 public immutable l2RecipientAddr;
 
-    uint256 constant SUBMIT_L1_BLOCKHASH_SELECTOR =
-        0x1d10503b9f4c7f4bb8f0b639ec987e6ffb203184507406bc0c0a591189f00ec;
+    uint256 constant SUBMIT_L1_BLOCKHASH_SELECTOR = 598342674068027518481179578557554850038206119856216505601406522348670006916;
 
     constructor(IStarknetCore starknetCore_, uint256 l2RecipientAddr_) {
         starknetCore = starknetCore_;
@@ -32,17 +19,24 @@ contract L1MessagesSender {
     function sendExactBlockHashToL2(uint256 blockNumber_) external {
         bytes32 blockHash = blockhash(blockNumber_);
         require(blockHash != bytes32(0), "ERR_INVALID_BLOCK_NUMBER");
-        _sendBlockHashToL2(blockHash);
+        _sendBlockHashToL2(blockHash, blockNumber_);
     }
 
     function sendLatestBlockHashToL2() external {
         bytes32 blockHash = blockhash(block.number - 1);
-        _sendBlockHashToL2(blockHash);
+        _sendBlockHashToL2(blockHash, block.number - 1);
     }
 
-    function _sendBlockHashToL2(bytes32 blockHash_) internal {
-        uint256[] memory message = new uint256[](1);
-        message[0] = uint256(blockHash_);
+    function _sendBlockHashToL2(bytes32 blockHash_, uint256 blockNumber_) internal {
+        uint256[] memory message = new uint256[](5);
+        (bytes8 hashWord1, bytes8 hashWord2, bytes8 hashWord3, bytes8 hashWord4) = FormatWords64.fromBytes32(blockHash_);
+
+        message[0] = uint256(uint64(hashWord1));
+        message[1] = uint256(uint64(hashWord2));
+        message[2] = uint256(uint64(hashWord3));
+        message[3] = uint256(uint64(hashWord4));
+        message[4] = blockNumber_;
+
         starknetCore.sendMessageToL2(l2RecipientAddr, SUBMIT_L1_BLOCKHASH_SELECTOR, message);
     }
 }
---
+++ tests/ethereum_l1_messages_sender.py
@@ -0,0 +1,26 @@
+from brownie import accounts, network
+from brownie.network.event import _decode_logs
+
+
+def test_l1_messages_sender_deploys(L1MessagesSender):
+    accounts[0].deploy(L1MessagesSender, accounts[0].address, 9847383783)
+
+def test_send_exact_block_hash_to_l2(L1MessagesSender, StarknetCoreMock):
+    starknet_core_mock = accounts[0].deploy(StarknetCoreMock)
+    l2_contract_addr = 9847383783
+    l1_messages_sender = accounts[0].deploy(L1MessagesSender, starknet_core_mock.address, l2_contract_addr)
+
+    latest_block = network.web3.eth.blockNumber
+    tx = l1_messages_sender.sendExactBlockHashToL2(latest_block, {'from': accounts[0]})
+
+    logs = _decode_logs(tx.logs)
+
+def test_send_latest_exact_block_hash_to_l2(L1MessagesSender, StarknetCoreMock):
+    starknet_core_mock = accounts[0].deploy(StarknetCoreMock)
+    l2_contract_addr = 9847383783
+    l1_messages_sender = accounts[0].deploy(L1MessagesSender, starknet_core_mock.address, l2_contract_addr)
+
+    tx = l1_messages_sender.sendLatestBlockHashToL2({'from': accounts[0]})
+    logs = _decode_logs(tx.logs)
+    
+
