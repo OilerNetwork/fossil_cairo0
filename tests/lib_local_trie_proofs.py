61b3a0010038b11f3ce2ba3257dfa98ed155ed80
Marcello Bardus <marcello@bardus.it>
2021-12-20 18:15:45 +0000

    shared prefix length WiP

--- tests/lib_local_trie_proofs.py
+++ tests/lib_local_trie_proofs.py
@@ -1,26 +1,109 @@
 import pytest
+from brownie import accounts
+from itertools import chain
+from collections import Counter
 
+from utils.helpers import (
+    hex_string_to_words64,
+    word64_to_nibbles,
+    hex_string_to_nibbles,
+    words64_to_nibbles
+)
+from utils.benchmarks.trie_proofs import merkle_patricia_input_decode, verify_account_proof
 from mocks.trie_proofs import trie_proofs
+from utils.rlp import extract_list_values, to_list
 
-from utils.encode_proof import encode_proof
-from utils.helpers import word64_to_nibbles, hex_string_to_words64
 
-from utils.benchmarks.trie_proofs import verify_account_proof
+def test_word64_to_nibbles_skip_0(TestTrieProofs):
+    test_trie_proofs = accounts[0].deploy(TestTrieProofs)
 
-# @pytest.mark.asyncio
-# async def test_encode_proof():
-#     account_proof_rlp = encode_proof(trie_proofs[0]['accountProof'])
-#     storage_proofs_rlp = list(map(lambda storage: encode_proof(storage['proof']), trie_proofs[0]['storageProof']))
+    input = '0x338cfc997a82252167ac25a16580d9730353eb1b9f0c6bbf0e4c82c4d0'
+    word64 = hex_string_to_words64(input)[0]
+    
+    output = word64_to_nibbles(word64, 16)
+    expected_output_hex = str(test_trie_proofs.decodeNibbles(input, 0))
+    expected_output = hex_string_to_nibbles(expected_output_hex)
+    assert expected_output[0:16] == output[0:16]
 
-# @pytest.mark.asyncio
-# async def test_word64_to_bytes():
-#     word = 12379813738319119365
-#     word64_to_nibbles(word, 15)
 
-# @pytest.mark.asyncio
-# async def test_decode_proof_to_words():
-#     account_proof_rlp = encode_proof(trie_proofs[0]['accountProof'])
-#     account_proof_rlp_words = rlp_string_to_words64(account_proof_rlp)
+def test_word64_to_nibbles_skip_1(TestTrieProofs):
+    test_trie_proofs = accounts[0].deploy(TestTrieProofs)
+
+    input = '0x338cfc997a82252167ac25a16580d9730353eb1b9f0c6bbf0e4c82c4d0'
+    words64 = hex_string_to_words64(input)
+    
+    output = words64_to_nibbles(words64, (len(input) - 2), 1)
+    expected_output_hex = str(test_trie_proofs.decodeNibbles(input, 1))
+    expected_output = hex_string_to_nibbles(expected_output_hex)
+
+    assert output[0:16] == expected_output[0:16]
+
+
+def test_word64_to_nibbles_skip_2(TestTrieProofs):
+    test_trie_proofs = accounts[0].deploy(TestTrieProofs)
+
+    input = '0x338cfc997a82252167ac25a16580d9730353eb1b9f0c6bbf0e4c82c4d0'
+    words64 = hex_string_to_words64(input)
+    
+    output = words64_to_nibbles(words64, (len(input) - 2), 2)
+    expected_output_hex = str(test_trie_proofs.decodeNibbles(input, 2))
+    expected_output = hex_string_to_nibbles(expected_output_hex)
+
+    assert output[0:16] == expected_output[0:16]
+
+
+def test_decode_nibbles_leaf_node(TestTrieProofs):
+    account_proof = trie_proofs[0]['accountProof']
+    leaf_node = account_proof[len(account_proof) - 1]
+
+    test_trie_proofs = accounts[0].deploy(TestTrieProofs)
+
+    expected_output = str(test_trie_proofs.decodeNibbles(leaf_node, 0))
+    output = list(map(lambda word: word64_to_nibbles(word, 16), hex_string_to_words64(leaf_node)))
+
+    assert Counter(chain.from_iterable(output)) == Counter(hex_string_to_nibbles(expected_output))
+
+
+def test_decode_nibbles_extension_node(TestTrieProofs):
+    account_proof = trie_proofs[0]['accountProof']
+    extension_node = account_proof[len(account_proof) - 2]
+
+    test_trie_proofs = accounts[0].deploy(TestTrieProofs)
+
+    expected_output = str(test_trie_proofs.decodeNibbles(extension_node, 0))
+    output = words64_to_nibbles(hex_string_to_words64(extension_node), len(extension_node) - 2)
+
+    assert Counter(output) == Counter(hex_string_to_nibbles(expected_output))
+
+
+def test_decode_nibbles_branch_node(TestTrieProofs):
+    account_proof = trie_proofs[0]['accountProof']
+    branch_node = account_proof[0]
+
+    test_trie_proofs = accounts[0].deploy(TestTrieProofs)
+
+    expected_output = str(test_trie_proofs.decodeNibbles(branch_node, 0))
+    output = words64_to_nibbles(hex_string_to_words64(branch_node), len(branch_node) - 2)
+
+    assert Counter(output) == Counter(hex_string_to_nibbles(expected_output))
+
+
+def test_merkle_patricia_decode_leaf(TestTrieProofs):
+    test_trie_proofs = accounts[0].deploy(TestTrieProofs)
+    account_proof = trie_proofs[0]['accountProof']
+    leaf_node = account_proof[len(account_proof) - 1]
+    leaf_node_64 = hex_string_to_words64(leaf_node)
+
+    leaf_node_items = to_list(leaf_node_64)
+    leaf_node_values = extract_list_values(leaf_node_64, leaf_node_items)
+
+    leaf_node_value = '0x338cfc997a82252167ac25a16580d9730353eb1b9f0c6bbf0e4c82c4d0'
+
+    output = merkle_patricia_input_decode(leaf_node_values[0], len(leaf_node_value) - 2)
+    expected_output = str(test_trie_proofs.merklePatriciaCompactDecode(leaf_node_value))
+
+    assert Counter(output) == Counter(hex_string_to_nibbles(expected_output))
+
 
 @pytest.mark.asyncio
 async def test_verify_invalid_account_proof():
@@ -28,6 +111,7 @@ async def test_verify_invalid_account_proof():
     root_hash = hex_string_to_words64('0x96c4bdfb8f2ad089200bad93f6216fe96652f9e2761b55bfd8a715ad3d6ecaf6')
     with pytest.raises(Exception):
         verify_account_proof(account, root_hash, [])
+
 
 @pytest.mark.asyncio
 async def test_verify_invalid_account_proof():
@@ -37,5 +121,4 @@ async def test_verify_invalid_account_proof():
     proof = [hex_string_to_words64(node)]
     proof_lens = [int((len(node) - 2) / 2)]
     res = verify_account_proof(account, root_hash, proof, proof_lens)
-
 
--- tests/utils/benchmarks/trie_proofs.py
+++ tests/utils/benchmarks/trie_proofs.py
@@ -1,20 +1,48 @@
 from typing import List, Callable, NamedTuple
 from web3 import Web3
 
-from utils.helpers import hex_string_to_words64, ints_array_to_bytes, keccak_words64
+from utils.helpers import hex_string_to_words64, keccak_words64, words64_to_nibbles, word64_to_nibbles
 from utils.rlp import extractData, getElement, to_list
 
 
-class LeafeNode(NamedTuple):
-    key: List[int]
-    value: List[int]
-
-class BranchNode(NamedTuple):
-    nib_0: LeafeNode
-    nib_1: LeafeNode
-    nib_2: LeafeNode
-
 EMPTY_TRIE_ROOT_HASH = "0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421"
+
+
+def merkle_patricia_input_decode(input: List[int], input_len_bytes: int) -> List[int]:
+    first_nibble = word64_to_nibbles(input[0], 16)[0]
+
+    skip_nibbles = 0
+
+    if first_nibble == 0:
+        skip_nibbles = 2
+    elif first_nibble == 1:
+        skip_nibbles = 1
+    elif first_nibble == 2:
+        skip_nibbles = 2
+    elif first_nibble == 3:
+        skip_nibbles = 1
+    else:
+        assert False
+
+    return words64_to_nibbles(input, input_len_bytes, skip_nibbles)
+
+
+def count_shared_prefix_len(
+    path_offset: int,
+    path: List[int],
+    path_len: int,
+    node_path: List[int],
+    node_path_len: int,
+    current_index: int = 0
+) -> int:
+    if current_index + path_offset >= path_len and current_index >= node_path_len:
+        return current_index
+    else:
+        if path[current_index + path_offset] != node_path[current_index]:
+            return current_index
+        else:
+            return count_shared_prefix_len(path_offset, path, path_len, node_path, node_path_len, current_index + 1)
+
 
 def verify_account_proof(
     account: List[int],
@@ -43,8 +71,6 @@ def verify_account_proof(
 
         # Handle leaf node
         if len(node) == 2:
+            pass
             # TODO wtf is div
         
-
-    
-    
