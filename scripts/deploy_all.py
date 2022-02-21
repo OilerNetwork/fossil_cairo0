05a4b5ef2563380a84dbb36727f0dbe7f1d54de6
Marcello Bardus <marcello@bardus.it>
2022-02-21 17:28:18 +0100

    Deployment script update to deploy TWAP and facts registry

--- scripts/deploy_all.py
+++ scripts/deploy_all.py
@@ -1,4 +1,5 @@
 import asyncio
+import pytest
 import json
 from services.external_api.base_client import RetryConfig
 
@@ -17,16 +18,16 @@ def get_gateway_client(gateway_url: str) -> GatewayClient:
     retry_config = RetryConfig(n_retries=1)
     return GatewayClient(url=gateway_url, retry_config=retry_config)
 
-
-async def main():
+@pytest.mark.asyncio
+async def test_deploy():
     secrets = json.load(open('secrets.json'))
-    gateway_url = "http://localhost:5000" #"https://alpha4.starknet.io/"
+    gateway_url = "https://alpha4.starknet.io/"
     eth_provider_url = f"https://eth-goerli.alchemyapi.io/v2/{secrets['alchemy_api_key']}"
 
     starknet_core_addr = '0xde29d060D45901Fb19ED6C6e959EB22d8626708e'
     gateway_client = get_gateway_client(gateway_url)
 
-    # msg_contract_definition = ContractDefinition.loads('contracts/starknet/L1MessagesProxy.cairo')
+    # Deploy L1Messages proxy - Starknet
     msg_dep_contract_tx = Deploy(
         contract_address_salt=fields.ContractAddressSalt.get_random_value(),
         constructor_calldata=[],
@@ -35,10 +36,11 @@ async def main():
             )
     )
 
-    msg_dep_gateway_response = await gateway_client.add_transaction(tx=msg_dep_contract_tx)
+    msg_dep_gateway_response = await gateway_client.add_transaction(msg_dep_contract_tx)
     l2_msg_contract_address = int(msg_dep_gateway_response["address"], 16)
     assert (msg_dep_gateway_response["code"] == StarkErrorCode.TRANSACTION_RECEIVED.name)
 
+    # Deploy L1HeadersStore - Starknet
     headers_dep_contract_tx = Deploy(
         contract_address_salt=fields.ContractAddressSalt.get_random_value(),
         constructor_calldata=[],
@@ -46,10 +48,11 @@ async def main():
                 files=['contracts/starknet/L1HeadersStore.cairo'], debug_info=True, cairo_path=['contracts']
             )
     )
-    headers_dep_gateway_response = await gateway_client.add_transaction(tx=headers_dep_contract_tx)
+    headers_dep_gateway_response = await gateway_client.add_transaction(headers_dep_contract_tx)
     l2_headers_contract_address = int(headers_dep_gateway_response["address"], 16)
     assert (headers_dep_gateway_response["code"] == StarkErrorCode.TRANSACTION_RECEIVED.name)
 
+    # Deploy L1 sender contract
     # Load compiled contract's bytecode and abi
     f = open('build/contracts/L1MessagesSender.json')
     contract_build = json.load(f)
@@ -60,7 +63,6 @@ async def main():
     # Load deployer account
     w3.eth.default_account = account
 
-    # Deploy
     L1MessagesSender = w3.eth.contract(abi=contract_build['abi'], bytecode=contract_build['bytecode'])
     deployment_tx = L1MessagesSender.constructor(starknet_core_addr, l2_msg_contract_address).buildTransaction({
         'from': account.address,
@@ -74,13 +76,15 @@ async def main():
     tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash)
     l1_contract_addr = tx_receipt.contractAddress
 
+    # Initialize L1HeadersStore contract - Starknet
     l2_headers_contract_init_tx = InvokeFunction(
             contract_address=l2_headers_contract_address,
             entry_point_selector=get_selector_from_name('initialize'),
             calldata=[l2_msg_contract_address],
             signature=[])
 
-    l2_msg_contract_owner = 7488483839384883
+    # Initialize and L1MessagingContract- Starknet
+    l2_msg_contract_owner = 0x02a57f0737b8c4beb1ddfb013f046520680a5f0e056505e6420c0ff827b0c45c
 
     l2_msg_contract_init_tx = InvokeFunction(
             contract_address=l2_msg_contract_address,
@@ -91,10 +95,50 @@ async def main():
     await gateway_client.add_transaction(l2_headers_contract_init_tx)
     await gateway_client.add_transaction(l2_msg_contract_init_tx)
 
+    # Deploy and initialize TWAP contract
+    twap_dep_contract_tx = Deploy(
+        contract_address_salt=fields.ContractAddressSalt.get_random_value(),
+        constructor_calldata=[],
+        contract_definition=compile_starknet_files(
+                files=['contracts/starknet/TWAP.cairo'], debug_info=True, cairo_path=['contracts']
+            )
+    )
+    twap_dep_gateway_response = await gateway_client.add_transaction(twap_dep_contract_tx)
+    twap_contract_address = int(twap_dep_gateway_response["address"], 16)
+    assert (twap_dep_gateway_response["code"] == StarkErrorCode.TRANSACTION_RECEIVED.name)
+    twap_contract_init_tx = InvokeFunction(
+            contract_address=twap_contract_address,
+            entry_point_selector=get_selector_from_name('initialize'),
+            calldata=[int(l1_contract_addr, 16)],
+            signature=[])
+    await gateway_client.add_transaction(twap_contract_init_tx)
+
+    # Deploy and initialize Facts Registry contract
+    facts_registry_dep_contract_tx = Deploy(
+        contract_address_salt=fields.ContractAddressSalt.get_random_value(),
+        constructor_calldata=[],
+        contract_definition=compile_starknet_files(
+                files=['contracts/starknet/FactsRegistry.cairo'], debug_info=True, cairo_path=['contracts']
+            )
+    )
+    facts_registry_dep_gateway_response = await gateway_client.add_transaction(facts_registry_dep_contract_tx)
+    facts_registry_contract_address = int(facts_registry_dep_gateway_response["address"], 16)
+    assert (facts_registry_dep_gateway_response["code"] == StarkErrorCode.TRANSACTION_RECEIVED.name)
+    facts_registry_contract_init_tx = InvokeFunction(
+            contract_address=facts_registry_contract_address,
+            entry_point_selector=get_selector_from_name('initialize'),
+            calldata=[int(l1_contract_addr, 16)],
+            signature=[])
+    await gateway_client.add_transaction(facts_registry_contract_init_tx)
+
     print('\n')
     print(f"L1: contract address: {l1_contract_addr}")
     print(f"Starknet: L1 headers contract address: {hex(l2_headers_contract_address)}")
     print(f"Starknet: L1 messages recipient: {hex(l2_msg_contract_address)}")
+    print(f"Starknet: Facts registry: {hex(facts_registry_contract_address)}")
+    print(f"Starknet: TWAP: {hex(twap_contract_address)}")
 
-asyncio.run(main())
+    assert 1 == 1
+
+# asyncio.run(main())
 
